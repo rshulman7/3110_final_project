7b299eeac17d12e2a5796f55b3e4cc7ff8738d1e
sy458 <sy458@cornell.edu>
2021-03-15 23:58:51 -0400

    Completed most of vector.ml

--- vector.ml
+++ vector.ml
@@ -10,25 +10,46 @@ type t = Vector of elt list * int
 
 exception Dimension_Mismatch
 
-let dim : t -> int = failwith "Unimplemented"
+let dim : t -> int = function Vector (_, d) -> d
 
-let add_elt : t -> elt -> t = failwith "Unimplemented"
+let from_reals_list elt_lst = Vector (elt_lst, List.length elt_lst)
 
-let of_reals_list = failwith "Unimplemented"
+let to_reals_list = function Vector (elt_lst, _) -> elt_lst
 
-let to_reals_list = failwith "Unimplemented"
+let add_elt (v : t) (e : elt) : t =
+  Vector (to_reals_list v @ [ e ], dim v + 1)
 
-let sum : t -> t -> t = failwith "Unimplemented"
+let sum (v_1 : t) (v_2 : t) : t =
+  if dim v_1 = dim v_2 then
+    let reals_1 = to_reals_list v_1 and reals_2 = to_reals_list v_2 in
+    Vector
+      (List.map2 (fun x y -> Reals.( +: ) x y) reals_1 reals_2, dim v_1)
+  else raise Dimension_Mismatch
 
-let dot : t -> t -> elt = failwith "Unimplemented"
+let dot (v_1 : t) (v_2 : t) : elt =
+  if dim v_1 = dim v_2 then
+    let reals_1 = to_reals_list v_1 and reals_2 = to_reals_list v_2 in
+    let dotted =
+      List.map2 (fun x y -> Reals.( *: ) x y) reals_1 reals_2
+    in
+    let rec sum_plus_acc (acc : elt) (elt_lst : elt list) : elt =
+      match elt_lst with
+      | [] -> acc
+      | h :: t -> sum_plus_acc (Reals.( +: ) acc h) t
+    in
+    sum_plus_acc Reals.Zero dotted
+  else raise Dimension_Mismatch
 
-let scalar_mult : t -> elt -> t = failwith "Unimplemented"
+let scalar_mult (v : t) (e : elt) : t =
+  Vector (to_reals_list v |> List.map (fun x -> Reals.( *: ) e x), dim v)
 
 let cross : t -> t -> t = failwith "Unimplemented"
 
-let subtract : t -> t -> t = failwith "Unimplemented"
+let subtract (v_1 : t) (v_2 : t) : t =
+  Reals.Float (-1.) |> scalar_mult v_2 |> sum v_1
 
-let lookup : t -> int -> elt = failwith "Unimplemented"
+let lookup (v : t) (index : int) : elt =
+  index |> List.nth (to_reals_list v)
 
 let norm = failwith "Unimplemented"
 

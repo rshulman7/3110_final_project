0ebbf89260b5088547c54b4710946adf903f4543
Ellie Eleftheria Fassman <oef3@cornell.edu>
2021-03-21 11:43:01 -0400

    Worked on: -test.ml
    Implemented:
    -test suite skeleton for parse_matrix

--- io.ml
+++ io.ml
@@ -1,8 +1,3 @@
-(* TODO: E: - implement : - val parse_size : string -> int * int - val
-   num_matrix : string -> int - val parse_matrix : string -> t list -
-   catch invalid inputs and call reprompt R: - val function_opt :
-   string-> t list - reprompt - getting REPL set up *)
-
 open Reals
 
 exception Invalid_input
--- test.ml
+++ test.ml
@@ -1,6 +1,41 @@
 open OUnit2
-open Testmath
-open Testrepl
+open Io
+
+(* open Testmath open Testrepl *)
+
+exception Invalid_input
+
+(* checks equality of 2 matrices *)
+let matrix_eq mat_a mat_b =
+  let lst_a = List.flatten mat_a in
+  let lst_b = List.flatten mat_b in
+  lst_a = lst_b
+
+(* helper function for io_tests which tests parse_matrix. Tests valid
+   input (i.e. no exn raised) if exn_bin = 0; otherwise tests if exn
+   raised *)
+let pm_test name exp_matrix input_str exn_bin =
+  if exn_bin = 0 then
+    "[parse_matrix] test: " ^ name >:: fun _ ->
+    assert (matrix_eq exp_matrix (parse_matrix input_str))
+  else
+    "[parse_matrix] exn test: " ^ name >:: fun _ ->
+    assert_raises Invalid_input (fun () -> parse_matrix input_str)
+
+(* mainly tests parse_matrix, with a few tests intially to parse_size
+   and num_matrix *)
+let io_tests =
+  [
+    pm_test "mixed 2 x 3"
+      [
+        [ Float 1.4; Rational (4, 3); Zero ];
+        [ Zero; Float 1.567; Float 1. ];
+      ]
+      "[1.4, 4/3, 0; 0/9, 1.567, 1]" 0;
+    pm_test "zeros 2 x 2"
+      [ [ Zero; Zero ]; [ Zero; Zero ] ]
+      "[0, 0; 0/7, 0.]" 0;
+  ]
 
 let suite =
   "test suite for project"

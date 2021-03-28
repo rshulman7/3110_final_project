aa06ece11cd96d61e8b1a8599ada5bb0a41c0fda
Ellie Eleftheria Fassman <oef3@cornell.edu>
2021-03-27 20:02:23 -0400

    Wrote better specs for io.mli

--- io.mli
+++ io.mli
@@ -1,18 +1,24 @@
-(** exception thrown for invalid strings passed to parse_matrix*)
+(** This module handles input strings and converts them into inputs
+    useable by the rest of the program *)
+
+(** exception thrown for invalid strings passed to parse_matrix *)
 exception Invalid_input
 
-(** converts a string input representing the dimensions of a matrix to
-    the dimensions *)
+(** [parse_size str] converts str representing the dimensions of a
+    matrix to the dimensions. Requires: str contains 2 integers *)
 val parse_size : string -> int * int
 
-(** converts a string input representing the number of a matrices (int)
-    to the number it represents *)
+(** [num_matrix str] converts str representing the number of matrices
+    (int) to its number. Requires: str contains 1 integer *)
 val num_matrix : string -> int
 
-(** converts a string input representing a matrix to the matrix of reals
-    it represents *)
+(** [parse_matrix str] converts str representing a matrix to the matrix
+    of reals it represents. Requires: str containing numbers within
+    square brackets, elements separated by commas, rows separated by
+    semicolons. Ex: "[5, 0; 6.7, -457]" -> [Rational (5, 1); Zero];
+    [Float 6.7; Rational (-457, 1)] *)
 val parse_matrix : string -> Reals.t list list
 
-(** converts a string input representing a real value to the real value
-    itself *)
+(** [parse_real str] converts str representing a real value to its real.
+    Requires: str represents a real *)
 val parse_real : string -> Reals.t

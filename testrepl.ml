0613fb8e6bac76f1566b39b7ea4f1676873b26ed
Ross Shulman <ser249@cornell.edu>
2021-04-24 13:16:20 -0400

    test equation parser

--- testrepl.ml
+++ testrepl.ml
@@ -14,7 +14,7 @@ let matrix_eq mat_a mat_b =
         | h1 :: t1 -> (
             match lst_b with
             | h2 :: t2 -> real_eq t1 t2 (h1 =: h2 && eq_val)
-            | [] -> eq_val )
+            | [] -> eq_val)
         | [] -> eq_val
       else eq_val
     in
@@ -47,6 +47,17 @@ let multi_printer lst_of_lsts =
   in
   "[" ^ print_helper lst_of_lsts ^ "]"
 
+(* for string list list *)
+let multi_printer2 lst_of_lsts =
+  let rec print_helper = function
+    | [] -> ""
+    | h :: t ->
+        pp_list (fun x -> x) h
+        ^ (if t = [] then "" else ";\n ")
+        ^ print_helper t
+  in
+  "[" ^ print_helper lst_of_lsts ^ "]"
+
 (* helper function for io_tests which tests parse_matrix. Tests valid
    input (i.e. no exn raised) if exn_bin = 0; otherwise tests if exn
    raised *)
@@ -58,9 +69,16 @@ let pm_test name exp_matrix input_str exn_bin =
   else
     "[parse_matrix] exn test: " ^ name >:: fun _ ->
     assert_equal "exn thrown"
-      ( match parse_matrix input_str with
+      (match parse_matrix input_str with
       | exception Io.Invalid_input -> "exn thrown"
-      | _ -> "" )
+      | _ -> "")
+
+let prime_tester name expected_rows expected_primes input =
+  "[parse_matrix] test: " ^ name >:: fun _ ->
+  make_rows input;
+  assert_equal ~printer:multi_printer2 expected_rows
+    input.processed_rows;
+  assert_equal expected_primes input.primes
 
 (* tests parse_matrix *)
 let io_tests =
@@ -118,6 +136,79 @@ let io_tests =
       "[5; 6.7, 3, -457   ; 0, 3.1415         ]" 0;
   ]
 
+let basic =
+  {
+    rows =
+      [ "x\'= 2.5 x+ 3y "; " y\' = 4x + 4.9 y + z"; "z\' =  4y + 2 z" ];
+    vars = [];
+    processed_rows = [];
+    primes = [];
+  }
+
+let unordered =
+  {
+    rows = [ "x\'= 2.5 x+ 3y "; " y\' = z + 4.9y +4x"; "z\' =  2z +4y" ];
+    vars = [];
+    processed_rows = [];
+    primes = [];
+  }
+
+let y =
+  {
+    rows = [ "x\' = 3y + 2.5x"; "y\' = z+4.9y+4x"; "z\' =  2z+4y" ];
+    vars = [];
+    processed_rows = [];
+    primes = [];
+  }
+
+let z =
+  {
+    rows = [ "z\' =  2z+4y"; "x\' = 3y + 2.5x"; "y\' = z+4.9y+4x" ];
+    vars = [];
+    processed_rows = [];
+    primes = [];
+  }
+
+let floats =
+  {
+    rows =
+      [
+        "z\' =  1.2345z+6.7y";
+        "x\' = 9876.543y + 2.5x";
+        "y\' = z+4444.4y+5555x";
+      ];
+    vars = [];
+    processed_rows = [];
+    primes = [];
+  }
+
+let prime_tests =
+  [
+    prime_tester
+      "test whitespace, case where variables are found in alphabetical \
+       order and variables are in order in each equation"
+      [ [ "2.5"; "3"; "0" ]; [ "4"; "4.9"; "1" ]; [ "0"; "4"; "2" ] ]
+      [ 'x'; 'y'; 'z' ] basic;
+    prime_tester "variables are not in order in each equation"
+      [ [ "2.5"; "3"; "0" ]; [ "4"; "4.9"; "1" ]; [ "0"; "4"; "2" ] ]
+      [ 'x'; 'y'; 'z' ] unordered;
+    prime_tester
+      "variables are found by parser in non-alphabetical order (first \
+       variable in first row is y, not x)"
+      [ [ "2.5"; "3"; "0" ]; [ "4"; "4.9"; "1" ]; [ "0"; "4"; "2" ] ]
+      [ 'x'; 'y'; 'z' ] y;
+    prime_tester "primes are not in order "
+      [ [ "0"; "4"; "2" ]; [ "2.5"; "3"; "0" ]; [ "4"; "4.9"; "1" ] ]
+      [ 'z'; 'x'; 'y' ] z;
+    prime_tester "many floats "
+      [
+        [ "0"; "6.7"; "1.2345" ];
+        [ "2.5"; "9876.543"; "0" ];
+        [ "5555"; "4444.4"; "1" ];
+      ]
+      [ 'z'; 'x'; 'y' ] floats;
+  ]
+
 (* don't change the name, add other test lists to the list as you make
    new test lists *)
-let test_list = List.flatten [ io_tests ]
+let test_list = List.flatten [ io_tests; prime_tests ]

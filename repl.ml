7412cab1180b2a8dca4b676b2479c1a47bc39700
Ellie Eleftheria Fassman <oef3@cornell.edu>
2021-04-27 20:51:24 -0400

    fixed reprompt for matrix ops

--- repl.ml
+++ repl.ml
@@ -153,7 +153,7 @@ let rec prompter () =
 (** [reader f] prompts user for inputs that are appropriate for function
     [f] and returns the result of calling [f] on those inputs. *)
 and reader f =
-  (match f with
+  ( match f with
   | TwoMatrix func -> (
       print_endline
         "We need to know the two matrices for this operation. Please \
@@ -164,7 +164,7 @@ and reader f =
       try matrix_answer (func matrix_a matrix_b)
       with _ ->
         print_string "There was an error. Check matrix dimensions. \n";
-        prompter ())
+        prompter () )
   | Scalar func -> (
       print_endline
         "We need to know the matrix for this operation. Please input \
@@ -175,7 +175,7 @@ and reader f =
       try matrix_answer (func scalar matrix_a)
       with _ ->
         print_string "There was an error. Check matrix dimensions \n";
-        prompter ())
+        prompter () )
   | Matrix func -> (
       print_endline
         "We need to know the matrix for this operation. Please input \
@@ -184,7 +184,7 @@ and reader f =
       try matrix_answer (func matrix_a)
       with _ ->
         print_string "There was an error. Check matrix dimensions \n";
-        prompter ())
+        prompter () )
   | MatrixVector func -> (
       print_endline
         "We need to know the matrix for this operation. Please input \
@@ -195,7 +195,7 @@ and reader f =
       try matrix_answer (func matrix_a vector)
       with _ ->
         print_string "There was an error. Check matrix dimensions. \n";
-        prompter ())
+        prompter () )
   | DiffyQ -> (
       print_string "Type your first expression and then press enter. ";
       let eqs : Io.eqs =
@@ -218,14 +218,14 @@ and reader f =
       with _ ->
         print_string
           "There was an error. Check that you used the correct syntax. \n";
-        prompter ())
+        prompter () )
   | Plotter -> (
       print_string "Please enter a 2 x n matrix. ";
       let matrix_a = matrix_parser (read_line ()) in
       try Plot.make_plot matrix_a
       with _ ->
         print_string "There was an error. Check matrix dimensions. \n";
-        prompter ())
+        prompter () )
   | MatrixOps ->
       print_string
         "Type your first matrix and assign it a name. Then press enter.";
@@ -233,12 +233,18 @@ and reader f =
       let x = ref (read_line ()) in
       while !x <> "done" do
         let old_lst = mat_eq.matrix_lst in
-        mat_eq.matrix_lst <- Io.make_mat_var !x :: old_lst;
-        print_string
-          "Type another matrix and assign it a name; then press enter. \
-           Or type 'done'";
-        x := read_line ()
+        try mat_eq.matrix_lst <- Io.make_mat_var !x :: old_lst
+        with _ ->
+          print_string
+            "There was an error. Check that you used the correct \
+             syntax. \n";
+          prompter ();
+          print_string
+            "Type another matrix and assign it a name; then press \
+             enter. Or type 'done'";
+          x := read_line ()
       done;
+
       mat_eq.matrix_lst <- List.rev mat_eq.matrix_lst;
       print_string
         "Type your equation using the matrix variables defined above. \
@@ -276,7 +282,7 @@ and reader f =
               constants. Any letter is an acceptable variable.\n";
              "\n******************************************\n";
            ])
-  | PromptAgain -> ());
+  | PromptAgain -> () );
   print_string "\n \n";
   prompter ()
 

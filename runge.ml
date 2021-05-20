b9de46c55c3d8e2dbd745a54181d61a66bbc3268
sy458 <sy458@cornell.edu>
2021-05-20 19:42:37 -0400

    runge code clean-up

--- runge.ml
+++ runge.ml
@@ -112,18 +112,6 @@ let rk_computation coef_matrix soln_matrix num_rows col step_size =
   done;
   ()
 
-(* let rk_computations soln_matrix col step_size coef constant = let
-   half = Reals.Rational (1, 2) in let sixth = Reals.Rational (1, 6) in
-   let prev_time = Matrix.lookup soln_matrix (0, col - 1) in let
-   prev_value = Matrix.lookup soln_matrix (1, col - 1) in let k1 = (coef
-   *: prev_value) +: constant in let k2 = (coef *: (prev_value +:
-   (step_size *: k1 *: half))) +: constant in let k3 = (coef *:
-   (prev_value +: (step_size *: k2 *: half))) +: constant in let k4 =
-   (coef *: (prev_value +: (step_size *: k3))) +: constant in
-   Matrix.change_matrix_value 0 col (prev_time +: step_size)
-   soln_matrix; Matrix.change_matrix_value 1 col (prev_value +: sixth *:
-   step_size *: (k1 +: (Reals.Float 2.0 *: k2) +: (Reals.Float 2.0 *:
-   k3) +: k4)) soln_matrix *)
 let rk plot m init_cond end_time step_size =
   if step_size <: Reals.Zero then raise Invalid_step_size
   else if end_time <: Reals.Zero then raise Invalid_end_time

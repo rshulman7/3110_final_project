a913ee136bebd35177af0f09cee330f4257c6fa3
Tom Schang <tms276@cornell.edu>
2021-05-24 12:43:44 -0400

    module plot documentation

--- plot.mli
+++ plot.mli
@@ -1,13 +1,14 @@
-(** This module handles all things related to plotting graphs and
-    solutions to the ODEs*)
+(** This module handles plotting graphs. *)
 
+(** The type of data from which plots are generated. *)
 type t = Matrix.t
 
+(** Raised when the data provided doesn't generate a valid plot. *)
 exception Invalid_plot
 
 (** [make_plot m] plots m with the first row x-values and second row
-    y-values
+    y-values.
 
     Raises: [Invalid_plot] if dimensions of the input [m] are not 2 x n
-    where n > 1*)
+    where n > 1. *)
 val make_plot : t -> unit

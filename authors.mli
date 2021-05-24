b5b6a2e427e8e1a9404ce81a32f9e433e1f7bb62
Tom Schang <tms276@cornell.edu>
2021-05-24 15:50:24 -0400

    author module fix

--- authors.mli
+++ authors.mli
@@ -4,6 +4,3 @@
     @author Ross Shulman (ser249)
     @author Tom Schang (tms276)
     @author Steve Yeh (sy458) *)
-
-(** [hours_worked] is the number of hours you worked on this assignment. *)
-val hours_worked : int
